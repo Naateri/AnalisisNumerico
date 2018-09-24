86fcd61e2ed15cf21094ac70f29b05c9ead49305
Nateri <renato.postigo@ucsp.edu.pe>
2018-09-24 16:31:00 -0500

    lagrange now returns the function in text too

--- LaGrange/backup/lagrange_pol.pas
+++ LaGrange/backup/lagrange_pol.pas
@@ -11,11 +11,14 @@ type
   TLagrange = class
     points: array of array of Real;
     num_points: Integer;
+    polinomy, real_polinomy: String;
 
     public
       constructor create;
       destructor Destroy; override;
       function find_pol(x: Real): Real;
+      function find_text(): String;
+//      function f(x: Real): Real;
   end;
 
 implementation
@@ -34,26 +37,62 @@ function TLagrange.find_pol(x: Real): Real;
 var
   i,j: Integer;
   temp, answer: Real;
+  temp_polinomy: String;
 begin
 //     SetLength(points, 2, num_points);
      temp := 1;
      answer := 0;
+     polinomy:= '';
      for i := 0 to num_points-1 do
      begin
        temp := 1;
+       temp_polinomy := '';
        for j := 0 to num_points - 1 do
        begin
-//         ShowMessage('idek' );
          if (i = j) then
             continue;
+         temp_polinomy := temp_polinomy + '( ( x - ' + FloatToStr(points[j][0]) + ')/' + FloatToStr( points[i][0] - points[j][0]) + ') )*';
          temp := temp * ( ( x - points[j][0] ) / ( points[i][0] - points[j][0]) );
        end;
        temp := temp * points[i][1];
+       temp_polinomy := FloatToStr(points[i][1]) + '*' + temp_polinomy;
+       polinomy := temp_polinomy + '+' + polinomy;;
        answer := answer + temp;
      end;
-     ShowMessage('answer: ' + FloatToStr(answer));
+//     ShowMessage('answer: ' + FloatToStr(answer));
+       real_polinomy:= polinomy;
      Result := answer;
 end;
+
+function TLagrange.find_text(): String;
+var
+  i,j: Integer;
+  temp, answer: Real;
+  temp_polinomy: String;
+begin
+//     SetLength(points, 2, num_points);
+     temp := 1;
+     answer := 0;
+     polinomy:= '';
+     for i := 0 to num_points-1 do
+     begin
+       temp := 1;
+       temp_polinomy := '';
+       for j := 0 to num_points - 1 do
+       begin
+         if (i = j) then
+            continue;
+         temp_polinomy := temp_polinomy + '( ( x - ' + FloatToStr(points[j][0]) + ')/' + FloatToStr( points[i][0] - points[j][0]) + ' )*';
+       end;
+       temp_polinomy := Copy(temp_polimomy, 0, Length(temp_polinomy) - 1);
+       temp_polinomy := FloatToStr(points[i][1]) + '*' + temp_polinomy;
+       polinomy := temp_polinomy + '+' + polinomy;;
+     end;
+//     ShowMessage('answer: ' + FloatToStr(answer));
+       real_polinomy:= Copy(polinomy, 0, Length(polinomy) - 1);
+     Result := real_polinomy;
+end;
+
 
 end.
 
--- LaGrange/main_lagrange.pas
+++ LaGrange/main_lagrange.pas
@@ -14,11 +14,16 @@ type
 
   TForm1 = class(TForm)
     btnGraph: TButton;
+    btnFindY: TButton;
     Chart1: TChart;
     Chart1FuncSeries1: TFuncSeries;
     EdiPuntos: TEdit;
+    ediX: TEdit;
+    ediYVal: TEdit;
     Label1: TLabel;
+    MemFunction: TMemo;
     StringGrid1: TStringGrid;
+    procedure btnFindYClick(Sender: TObject);
     procedure btnGraphClick(Sender: TObject);
     procedure Chart1FuncSeries1Calculate(const AX: Double; out AY: Double);
     procedure FormCreate(Sender: TObject);
@@ -42,7 +47,7 @@ procedure TForm1.Chart1FuncSeries1Calculate(const AX: Double; out AY: Double);
 var
   i,j: Integer;
 begin
-     LaGrange := TLagrange.create;
+{     LaGrange := TLagrange.create;
      LaGrange.num_points := StrToInt(EdiPuntos.Text);
      //SetLength(pts, 2, LaGrange.num_points);
      SetLength(pts, LaGrange.num_points, 2);
@@ -54,33 +59,49 @@ begin
        end;
      end;
      LaGrange.points := pts;
-
+     MemFunction.Clear;
+     MemFunction.Append(LaGrange.polinomy);
+     ShowMessage(LaGrange.polinomy);
+ }
      AY := LaGrange.find_pol(AX);
 end;
 
 procedure TForm1.btnGraphClick(Sender: TObject);
 var i, j: Integer;
+  func: String;
 begin
-{     LaGrange.num_points := StrToInt(EdiPuntos.Text);
-     //SetLength(pts, 2, LaGrange.num_points);
-     SetLength(pts, LaGrange.num_points, 2);
-     for i := 0 to LaGrange.num_points - 1 do
-     begin
-       for j := 0 to 1 do
-       begin
-         pts[i][j] := StrToFloat(StringGrid1.Cells[j,i]);
-       end;
-     end;
-     LaGrange.points := pts;
-}
+//     LaGrange := TLagrange.create;
+          LaGrange.num_points := StrToInt(EdiPuntos.Text);
+          //SetLength(pts, 2, LaGrange.num_points);
+          SetLength(pts, LaGrange.num_points, 2);
+          for i := 0 to LaGrange.num_points - 1 do
+          begin
+            for j := 0 to 1 do
+            begin
+              pts[i][j] := StrToFloat(StringGrid1.Cells[j,i]);
+            end;
+          end;
+          LaGrange.points := pts;
+          MemFunction.Clear;
+          func := LaGrange.find_text();
+          MemFunction.Append(func);
+//          ShowMessage(func);
+
      Chart1FuncSeries1.Active:= False;
      Chart1FuncSeries1.Pen.Color := clBlue;
      Chart1FuncSeries1.Active:= True;
 end;
 
+procedure TForm1.btnFindYClick(Sender: TObject);
+var x: Real;
+begin
+      x := StrToFloat(ediX.Text);
+      ediYVal.Text := FloatToStr(LaGrange.find_pol(x));
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-//  LaGrange := TLagrange.create;
+  LaGrange := TLagrange.create;
 end;
 
 end.
